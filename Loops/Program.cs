a144e46a20ae88719a86d428daa0ee432b09ecc3
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-18 12:35:00 -0300

    Added a loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,47 @@ namespace Loops
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Before Loops");
+			//Display number 1 - 10 
+
+			Console.WriteLine(1);
+			Console.WriteLine(2);
+			Console.WriteLine(3);
+			Console.WriteLine(4);
+			Console.WriteLine(5);
+			Console.WriteLine(6);
+			Console.WriteLine(7);
+			Console.WriteLine(8);
+			Console.WriteLine(9);
+			Console.WriteLine(10);
+
+
+			Console.WriteLine("Another way Before Loops");
+
+			//Display number 1 - 10
+			int count;
+			count = 1;
+
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1; 
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+			Console.WriteLine(count); count = count + 1;
+
+			Console.WriteLine("Loops");
+
+			Console.ReadKey(true);
 		}
 	}
 }
